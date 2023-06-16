8ecc6ff709b92c1a33dfb68a3b6e4ced37398df6
evan-mbp-2022 <public@evandaniel.com>
2023-06-16 03:13:28 -0500

    create project aspect container for padding sizing

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,24 +2,27 @@ const Project = props => {
     return (
         <>
             <div className={ `project ${ props.slug }`}>
-                <img className="image" src={ Object.values(props.img)[0] } />
+                <div className="aspect">
+                    <img className="image" src={ Object.values(props.img)[0] } />
 
-                <div className="lunette">
-                    <div className="description">
-                        { props.description }
-                    </div>
+                    <div className="lunette">
+                        <div className="description">
+                            { props.description }
+                        </div>
 
-                    <div className="links">
-                        <a href={ props.pdf } className="pdf">documentation</a>
-                        {props.link && 
-                            <a href={ props.link } className="link"> | link</a>
-                        }
-                    </div>
-                    
-                    <div className="title">
-                        { props.title }
+                        <div className="title">
+                            { props.title }
+                        </div>
+                        
+                        <div className="links">
+                            <a href={ props.pdf } className="pdf">documentation</a>
+                            {props.link && 
+                                <a href={ props.link } className="link"> | link</a>
+                            }
+                        </div>
                     </div>
                 </div>
+                
             </div>
         </>
     )
